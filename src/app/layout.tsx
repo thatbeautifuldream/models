920036ca345c5f3b5c82ad4f67612b38c223947d
Milind Mishra <milind.mishra4@gmail.com>
2025-09-14 05:36:45 +0530

    feat: oppsie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { env } from "@/env";
 import "./globals.css";
+import { ClarityProvider } from "@/components/providers/clarity-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,6 +48,7 @@ export default function RootLayout({
           </ThemeProvider>
         </NuqsAdapter>
         <GoogleAnalytics gaId={env.GA_ID} />
+        <ClarityProvider />
       </body>
     </html>
   );
